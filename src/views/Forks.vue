db45b528f2b533dea190e3f207f2131f3217b059
maksak_il <maksak_il@mail.ru>
2022-01-15 14:00:46 +0300

    Update Forks.vue

--- src/views/Forks.vue
+++ src/views/Forks.vue
@@ -144,7 +144,34 @@
       </div>
       <template v-else> Форков не найдено </template>
     </div>
-    <div v-if="loading" class="text-4xl">...Loading...</div>
+    <div
+      class="flex gap-2 items-center justify-center mt-10 mx-auto"
+      v-if="loading"
+    >
+      <svg
+        class="animate-spin -ml-1 mr-3 h-5 w-5 text-indigo-500"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          class="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        ></circle>
+        <path
+          class="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+      <span class="text-4xl font-extrabold text-gray-300 tracking-widest"
+        >Loading...</span
+      >
+    </div>
 
     <modal :show-modal="showModal" @close="hideFavoriteModal">
       <template v-if="!favorite.includes(active)">
@@ -288,7 +315,7 @@ export default {
 <style>
 .fade-enter-active,
 .fade-leave-active {
-  @apply transition;
+  @apply transition duration-300;
 }
 
 .fade-enter-from,
